4554da23cdcca9a722078df8c71d17bb5c910bbe
Arans <2403354941@qq.com>
2019-11-13 01:28:17 +0800

    feat: 我的资料页面和商品详情页面(db/dao中仅行数不同，直接merge即可)

---
+++ app/src/main/java/com/example/shopmanager/activities/BookDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.shopmanager.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.shopmanager.R;
+
+public class BookDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailbooks);
+
+    }
+}
