a4480b05485f162f8e8a9c0c5887314cf321f3c0
StarSky <254143170@qq.com>
2019-11-12 22:22:12 +0800

    书籍详细信息 greendao控件集成

---
+++ app/src/main/java/com/example/shopmanager/service/BookInfoService.java
@@ -0,0 +1,48 @@
+package com.example.shopmanager.service;
+
+
+import com.example.shopmanager.service.db.bean.BookInfo;
+import com.example.shopmanager.service.db.dao.BookInfoDao;
+
+import java.util.List;
+
+/**
+ * 书详情的数据请求层
+ */
+public class BookInfoService extends BaseService {
+
+    /**
+     *  获取全部图书信息
+     */
+    public List<BookInfo> queryBookInfoList() {
+        BookInfoDao bookInfoDao = daoSession.getBookInfoDao();
+        List<BookInfo> list = bookInfoDao.queryBuilder().list();
+        return list;
+    }
+
+    /**
+     *  根据图书编号获得图书信息
+     */
+    public BookInfo getBookInfByCode(String code) {
+        BookInfoDao bookInfoDao = daoSession.getBookInfoDao();
+        BookInfo bookInfo = bookInfoDao.queryBuilder().where(BookInfoDao.Properties.Code.eq(code)).unique();
+        return bookInfo;
+    }
+
+    /**
+     *  根据图书名字获得图书信息
+     */
+    public BookInfo getBookInfoByName(String name){
+        BookInfoDao bookInfoDao = daoSession.getBookInfoDao();
+        BookInfo bookInfo = bookInfoDao.queryBuilder().where(BookInfoDao.Properties.BookNmae.like("%"+name+"%")).unique();
+        return bookInfo;
+    }
+
+    /**
+     *  设定图书信息
+     */
+    public void setBookInfo(BookInfo bookInfo){
+        BookInfoDao bookInfoDao = daoSession.getBookInfoDao();
+        bookInfoDao.insertOrReplace(bookInfo);
+    }
+}
