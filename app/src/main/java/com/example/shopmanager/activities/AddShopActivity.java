52ffc8a7a749468a2a394bdfeb4b26b4444f33cd
StarSky <254143170@qq.com>
2019-11-17 20:56:58 +0800

    订单

--- app/src/main/java/com/example/shopmanager/activities/AddShopActivity.java
+++ app/src/main/java/com/example/shopmanager/activities/AddShopActivity.java
@@ -9,6 +9,19 @@ import androidx.annotation.Nullable;
 import com.example.shopmanager.R;
 
 public class AddShopActivity extends Activity {
+
+    private TextView et_add_book_code;
+    private TextView et_add_book_bookName;
+    private TextView et_add_book_price;
+    private TextView et_add_book_oldPrice;
+    private TextView et_add_book_author;
+    private TextView et_add_book_press;
+    private TextView et_add_book_code;
+    private TextView et_add_book_code;
+    private TextView et_add_book_code;
+    private TextView et_add_book_code;
+    private TextView et_add_book_code;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +31,16 @@ public class AddShopActivity extends Activity {
     }
 
     private void initView() {
-        (TextView) findViewById(R.id.);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
+        et_add_book_bookName = (TextView) findViewById(R.id.et_add_book_bookName);
+        et_add_book_price = (TextView) findViewById(R.id.et_add_book_price);
+        et_add_book_oldPrice = (TextView) findViewById(R.id.et_add_book_oldPrice);
+        et_add_book_author = (TextView) findViewById(R.id.et_add_book_author);
+        et_add_book_press = (TextView) findViewById(R.id.et_add_book_press);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
+        et_add_book_code = (TextView) findViewById(R.id.et_add_book_code);
     }
 }
