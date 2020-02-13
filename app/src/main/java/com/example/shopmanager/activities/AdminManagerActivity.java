9c8948e431dab1124cef9149565dfb2d16484003
Domineerxzx <37863484+Domineerxzx@users.noreply.github.com>
2020-02-13 20:36:17 +0800

    管理员管理页面

---
+++ app/src/main/java/com/example/shopmanager/activities/AdminManagerActivity.java
@@ -0,0 +1,17 @@
+package com.example.shopmanager.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.shopmanager.R;
+
+public class AdminManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_manager);
+    }
+}
